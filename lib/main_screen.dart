1639f2d15e4af81b3859dc1c5fc6571df10fcee3
PadmaGnanapriya <padmaisuru@gmail.com>
2021-10-19 10:43:30 +0530

    create read student identity card input.

---
+++ lib/main_screen.dart
@@ -0,0 +1,12 @@
+// importing dart:io file
+import 'dart:io';
+
+void main()
+{
+  print("Enter your University ID?");
+  // Reading name of the Geek
+  String name = stdin.readLineSync();
+
+  // Printing the name
+  print("Hello, $name! \nWelcome to GPA Calculator!!");
+}
