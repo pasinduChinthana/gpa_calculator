0b0d257f21fb995383b65253c192ec6b55ead0da
Pasindu Chinthana <edpchinthana@outlook.com>
2021-10-28 23:49:14 +0530

    create profile first form

--- lib/screens/create_profile_screen.dart
+++ lib/screens/create_profile_screen.dart
@@ -9,16 +9,61 @@ class CreateProfileScreen extends StatefulWidget {
 }
 
 class _CreateProfileScreenState extends State<CreateProfileScreen> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
           body: SingleChildScrollView(
-            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
             child: Form(
+              key: _formKey,
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text('Create Profile', style: Theme.of(context).textTheme.headline1,)
+                  Text('Create Profile', style: Theme.of(context).textTheme.headline1,),
+                  const SizedBox(height: 30),
+                  const Text('Profile Name'),
+                  TextFormField(
+                    validator: (value) {
+                      if(value == null || value.isEmpty) {
+                        return 'Please enter profile name';
+                      }
+                      return null;
+                    }
+                  ),
+                  const SizedBox(height: 20),
+                  const Text('University'),
+                  TextFormField(
+                      validator: (value) {
+                        if(value == null || value.isEmpty) {
+                          return 'Please enter university name';
+                        }
+                        return null;
+                      }
+                  ),
+                  const SizedBox(height: 20),
+                  const Text('Maximum GPA'),
+                  TextFormField(
+                      keyboardType: TextInputType.number,
+                      validator: (value) {
+                        if(value == null || value.isEmpty) {
+                          return 'Please enter maximum GPA';
+                        }
+                        return null;
+                      }
+                  ),
+                  const SizedBox(height: 30),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: ElevatedButton(
+                      onPressed: (){
+
+                      },
+                      child: const Text('Next')
+                    ),
+                  )
                 ],
               ),
             ),
